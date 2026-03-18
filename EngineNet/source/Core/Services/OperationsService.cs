0ef22f47905a30b17eb9edb746d852b424eb6070
samarixum <66809702+samarixum@users.noreply.github.com>
2026-03-18 14:18:11 +1100

    [v2.16.28-A] changes: 3, moved `GitService` and `GitTools` into Git folder Files changed in this commit: 	renamed:   EngineNet/source/Core/Services/Git/GitService.cs 	renamed:   EngineNet/source/Core/Services/Git/GitTools.cs 	modified:   EngineNet/source/Core/Services/OperationsService.cs

--- EngineNet/source/Core/Services/OperationsService.cs
+++ EngineNet/source/Core/Services/OperationsService.cs
@@ -1,9 +1,6 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using EngineNet.Core.Data;
 using EngineNet.Core.Utils;
-
 
 namespace EngineNet.Core.Services;
 
